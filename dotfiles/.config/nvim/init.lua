4878a8b1cc3a3e48c6311366465fae226ffc93c1
bokutotu <mushin.hudoushin@gmail.com>
2022-07-09 17:33:25 +0900

    add lualine

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,3 +1,4 @@
 require('plugins')
 require('basic')
 require('rc.bufferline')
+require('rc.lualine')
--- dotfiles/.config/nvim/lua/plugins.lua
+++ dotfiles/.config/nvim/lua/plugins.lua
@@ -18,5 +18,9 @@ return require('packer').startup({function()
   -- buffer line
   use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
 
-  -- ...
+  -- status line
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
 end, config = {compile_path = util.join_paths(vim.fn.stdpath('config'), 'packer_compiled.vim')}});
